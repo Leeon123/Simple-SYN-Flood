b97126aafb7888f9990a7366b53d992a310c19f7
Leeon123 <leeon123ex@gmail.com>
2018-12-08 22:34:50 +0800

    Update SYN-Flood.py

--- SYN-Flood.py
+++ SYN-Flood.py
@@ -30,7 +30,7 @@ def run():
     ack_flag = 0
     psh_flag = 0
     rst_flag = 0
-    syn_flag = 1
+    syn_flag = 1 # 1 is using syn flag , you can change these flag.
     fin_flag = 0
     tcp_flags = fin_flag + (syn_flag << 1) + (rst_flag << 2) + (psh_flag << 3) + (ack_flag << 4) + (urg_flag << 5)
     window_size = 65535
